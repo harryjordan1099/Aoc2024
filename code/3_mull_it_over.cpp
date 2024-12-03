109c8c5d9b5c3053a69f3b01b4f897e7361e251a
Harry <harry.jordan@hotmail.co.uk>
2024-12-03 21:19:35 +0000

    Complete part 2 of q3

--- code/3_mull_it_over.cpp
+++ code/3_mull_it_over.cpp
@@ -7,17 +7,27 @@
 #include <regex>
 #include <numeric>
 
+
+// This function is used to calculate the product from the regex
 int extractNumbers(const std::string& input) {
-    size_t start = input.find('(') + 1; // Find the '(' and move one position forward
+    // Find the '(' and move one position forward to fidn the first digit
+    size_t start = input.find('(') + 1; 
     size_t end = input.find(')');       // Find the ')'
 
+    // Extract the numbers first as a substring
     std::string numbers = input.substr(start, end - start);
 
+    /* 
+    string stream allows for extraction of individual values
+    whereas idnexing would be a ballache, we can use stringstream to automatically
+    parse double digit numbers and such 
+    */
     std::stringstream ss(numbers);
     int num1, num2;
     char comma;
 
     // Extract the two integers separated by a comma
+    // ss extracts first integer, then the comma, then the second integer
     ss >> num1 >> comma >> num2;
 
     int product = num1 * num2;
@@ -33,27 +43,36 @@ std::string readFileInput() {
         return ""; // Return an empty string if the file cannot be opened
     }
 
-    // Read the file content into a string
-    std::ostringstream buffer;
+    // Read the file content into a string, you can use ostringstream here as 
+    // we are just outputting, vs stringstream which does read a write
+    std::stringstream buffer;
     buffer << inputFile.rdbuf(); // Read all data from the file into the buffer
     return buffer.str();         // Convert the buffer to a string
 }
 
 int main() {
     std::string input = readFileInput();
+    // std::string input = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))";
+    std::regex pattern(R"regex((mul\(\d+,\d+\))|(do\(\))|(don't\(\)))regex");
 
-    std::regex pattern(R"(mul\(\d+,\d+\))");
 
     std::sregex_iterator it(input.begin(), input.end(), pattern);
     std::sregex_iterator end;
 
     std::vector<int> matches;
 
-    while (it != end) {
-        std::string string_output = it->str();
-        int int_output = extractNumbers(string_output);
+    bool do_or_dont = true;
 
-        matches.push_back(int_output);
+    while (it != end) {
+        std::smatch match = *it;
+        if (match[1].matched && do_or_dont) {
+            int int_output = extractNumbers(match[1].str());
+            matches.push_back(int_output);
+        } else if (match[2].matched) {
+            do_or_dont = true;
+        } else if (match[3].matched) {
+            do_or_dont = false;
+        }
         ++it;
         
     }
